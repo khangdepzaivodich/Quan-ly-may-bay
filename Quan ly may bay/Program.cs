470c145be58a90fee13f0718deff3b6f9769e20e
NguyenBaoHuy05 <baohuy30122005@gmail.com>
2024-12-15 10:44:32 +0700

    svfby

--- Quan ly may bay/Program.cs
+++ Quan ly may bay/Program.cs
@@ -17,7 +17,7 @@ namespace Quan_ly_may_bay
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ThongkeTaichinh());
+            Application.Run(new ReportChuyenBay("HK00000001"));
         }
     }
 }
